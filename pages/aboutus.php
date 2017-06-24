ebf6ba5f217efabd9d271241c6ca856151fab864
SimShauYuh <impure8987@gmail.com>
2017-06-24 16:00:17 +0800

    beiiging project

---
+++ pages/aboutus.php
@@ -0,0 +1,165 @@
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <a class="navbar-brand pull-right" href="https://twitter.com/amit_merchant">Twitter</a>
+          <a class="navbar-brand" href="#">Markdownify</a>
+        </div>
+      </div>
+    </nav>
+
+    <!-- Main jumbotron for a primary marketing message or call to action -->
+    <div class="jumbotron">
+      <div class="container text-center">
+        <h1>Markdownify</h1>
+        <div>
+        <p><img src="https://raw.githubusercontent.com/amitmerchant1990/electron-markdownify/master/img/markdownify.png" class="img-responsive logo-image text-center"></p>
+          <p>
+            <i class="tagline">A minimal Markdown editor</i>
+          </p>
+        </div>
+        <div>
+          <a href="https://github.com/amitmerchant1990/electron-markdownify/releases/tag/v1.2.0" class="btn btn-primary btn-lg">Download</a>
+          <a href="https://github.com/amitmerchant1990/electron-markdownify/" class="btn btn-danger btn-lg">GitHub</a>
+        </div>
+      </div>
+      <div class="text-center">
+        <img src="https://raw.githubusercontent.com/amitmerchant1990/electron-markdownify/master/img/markdownify.gif" class="img-responsive custom-image">
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="row text-center">
+        <h2 class="key_features">Key Features</h2>
+      </div>
+      <div class="row">
+        <div class="col-md-4">
+          <h3>LivePreview</h3>
+          <p> <b>Make changes, See changes</b>. Instantly see what your Markdown documents look like in HTML as you create them. </p>
+        </div>
+        <div class="col-md-4">
+          <h3>Sync Scrolling</h3>
+          <p>While you type, <b>LivePreview</b> will automatically scroll to the current location you're editing. </p>
+       </div>
+        <div class="col-md-4">
+          <h3>GFM</h3>
+          <p>The good old <b>GitHub Flavored-style</b> Markdown. So, you don't feel out of the place.</p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-4">
+          <h3>Syntax highlighting</h3>
+          <p> Syntax highlighting in fenced code blocks with language identifiers and Markup languages  </p>
+        </div>
+        <div class="col-md-4">
+          <h3>Dark/Light mode</h3>
+          <p>Change the color scheme of the editor based on your preferences. i.e to choose between Dark mode/Light mode </p>
+       </div>
+        <div class="col-md-4">
+          <h3>Emoji support</h3>
+          <p>Supports emojis in the preview mode for when you need to express yourself via one</p>
+        </div>
+      </div>
+    </div>
+
+    <hr>
+
+    <div class="container">
+      <div class="row text-center">
+        <div>
+          <h4>Other features including...</h4>
+          <ul class="center" type="square">
+            <li>Toolbar for basic Markdown formatting</li>
+            <li>Save the Markdown preview as PDF</li>
+            <li>App will keep alive in tray for quick usage</li>
+            <li>Full screen mode</li>
+            <ul>
+              <li>Write distraction free.</li>
+            </ul>
+            <li>Cross platform</li>
+            <ul>
+              <li>Windows, Mac and Linux ready.</li>
+            </ul>
+          </ul>
+        </div>
+
+      </div>
+    </div>
+
+    <hr>
+
+    <div class="container">
+          <div class="text-center">
+            <h2 class="key_features">Develop</h2>
+          </div>
+          <div>
+              <p>Install it using : <code>npm install electron-markdownify</code></p>
+              <p>And then run:</p>
+          </div>
+          <div>
+            <pre>
+              <code class="bash">
+$> cd PATH-TO-MARKDOWNIFY-DESKTOP-APP
+$> npm install && npm start
+              </code>
+            </pre>
+          </div>
+    </div>
+
+    <hr>
+
+    <div class="container">
+      <div class="row text-center credits">
+        <div>
+          <h2 class="key_features">Credits</h2>
+          <p>Markdowinfy is built using <a href="http://electron.atom.io/">Electron</a>, <a href="https://nodejs.org/">Node.js</a> and some <a href="https://github.com/amitmerchant1990/electron-markdownify#credits">more</a>.</p>
+        </div>
+      </div>
+    </div>
+
+    <hr>
+
+    <footer>
+      <p class="text-center">© 2016 <a href="https://www.amitmerchant.com/">Amit Merchant</a>.</p>
+    </footer>
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
+<div class="container">
+  <div class="row">
+    <div class="col-lg-3 col-sm-6">
+
+            <div class="card hovercard">
+                <div class="cardheader">
+
+                </div>
+                <div class="avatar">
+                    <img alt="" src="http://lorempixel.com/100/100/people/9/">
+                </div>
+                <div class="info">
+                    <div class="title">
+                        <a target="_blank" href="http://scripteden.com/">Script Eden</a>
+                    </div>
+                    <div class="desc">Passionate designer</div>
+                    <div class="desc">Curious developer</div>
+                    <div class="desc">Tech geek</div>
+                </div>
+                <div class="bottom">
+                    <a class="btn btn-primary btn-twitter btn-sm" href="https://twitter.com/webmaniac">
+                        <i class="fa fa-twitter"></i>
+                    </a>
+                    <a class="btn btn-danger btn-sm" rel="publisher"
+                       href="https://plus.google.com/+ahmshahnuralam">
+                        <i class="fa fa-google-plus"></i>
+                    </a>
+                    <a class="btn btn-primary btn-sm" rel="publisher"
+                       href="https://plus.google.com/shahnuralam">
+                        <i class="fa fa-facebook"></i>
+                    </a>
+                    <a class="btn btn-warning btn-sm" rel="publisher" href="https://plus.google.com/shahnuralam">
+                        <i class="fa fa-behance"></i>
+                    </a>
+                </div>
+            </div>
+
+    </div>
+
+  </div>
+</div>
